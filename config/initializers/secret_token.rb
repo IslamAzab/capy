53abe61b781901a0ad010e6cf5c054812b9a41fa
Islam Azab <islam.azab@badrit.com>
2013-06-10 16:25:21 +0200

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capy::Application.config.secret_token = '2822fd0dab24ac6b415888f2931d26af8c07d3301fa49b9c908b5fdec4f9e3ff057b0c797190dc54ab5a85a13a41ab1a8783dc4beaa0179704447ab7de56e74a'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Capy::Application.routes.draw do
+  root :to => "home#index"
+end
